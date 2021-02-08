330e3384ac289c387aa6fa11070522e2aab90807
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-02-08 21:37:06 +0300

    add array

--- main.js
+++ main.js
@@ -14,4 +14,24 @@ console.log('1' + test);
 //12
 console.log(test);
 console.log(test - 1);
-//1
+// data array
+const arr = [2, 3, 5, 8];
+let result = 1;
+for (let i = 0; i < arr.length; i += 1) {
+}
+console.log(result);
+const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] > 5 && arr2[i] < 10) {
+        console.log(arr2[i]);
+    }
+}
+const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
+let z = 0;
+for (let i = 0; i < arr3.length; i++) {
+    z = arr3[i] % 2;
+    if (z === 0) {
+        console.log(arr3[i]);
+    }
+}
+
