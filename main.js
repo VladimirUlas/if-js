a6820d3302c0856f92f76aea64c76b61fa72f039
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-02-16 21:02:26 +0300

    lesson 4

--- main.js
+++ main.js
@@ -1,38 +1,20 @@
-let user = 'John Doe';
-console.log(user);
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const para1 = document.getElementById('text1');
+const para2 = document.getElementById('text2');
+const para3 = document.getElementById('text3');
 
-const student = 'Vladimir';
-console.log(student);
-
-user = student;
-console.log(user);
-// In user name Vladimir
-let test = 1
-console.log(test);
-console.log(++test);
-console.log('1' + test);
-//12
-console.log(test);
-console.log(test - 1);
-// data array
-const arr = [2, 3, 5, 8];
-let result = 1;
-for (let i = 0; i < arr.length; i += 1) {
-    result *= arr[i];
-}
-console.log(result);
-const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (let i = 0; i < arr2.length; i++) {
-    if (arr2[i] > 5 && arr2[i] < 10) {
-        console.log(arr2[i]);
+function colorChange() {
+  let click = 0;
+  return function (event) {
+    click += 1;
+    const paragraf = event.target;
+    paragraf.style.color = colors[click - 1];
+    if (click > colors.lenght - 1) {
+      click = 0;
     }
-}
-const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-let z = 0;
-for (let i = 0; i < arr3.length; i++) {
-    z = arr3[i] % 2;
-    if (z === 0) {
-        console.log(arr3[i]);
-    }
+  };
 }
 
+para1.addEventListener('click', colorChange());
+para2.addEventListener('click', colorChange());
+para3.addEventListener('click', colorChange());
