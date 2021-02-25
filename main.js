9a2bda9bf33e3130d31677a79c605cbbadb59119
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-02-25 12:34:16 +0300

    Lesson 4 (#4)
    * lesson 4
    
    * Homework4
    
    * new homework4
    
    * homework4
    
    Co-authored-by: nikitik-nikita <nik.ikraaa@gmail.com>

--- main.js
+++ main.js
@@ -79,3 +79,24 @@ function zero() {
 }
 
 zero();
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const para1 = document.getElementById('text1');
+const para2 = document.getElementById('text2');
+const para3 = document.getElementById('text3');
+
+function colorChange() {
+    let click = 0;
+    return function (event) {
+        click += 1;
+        const paragraph = event.target;
+        paragraph.style.color = colors[click - 1];
+        if (click > colors.length - 1) {
+            click = 0;
+        }
+    }
+}
+
+para1.addEventListener('click', colorChange());
+para2.addEventListener('click', colorChange());
+para3.addEventListener('click', colorChange());
