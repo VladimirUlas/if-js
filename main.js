383f45874bbbfaae19824a296ebf195cd008f843
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-03-04 20:15:19 +0300

    Lesson 5 (#5)
    * Homework5/1
    
    * Homework_5
    
    * homework5
    
    Co-authored-by: nikitik-nikita <nik.ikraaa@gmail.com>

--- main.js
+++ main.js
@@ -100,3 +100,75 @@ function colorChange() {
 para1.addEventListener('click', colorChange());
 para2.addEventListener('click', colorChange());
 para3.addEventListener('click', colorChange());
+
+const data = [
+    {
+        country: 'Russia',
+        city: 'Saint Petersburg',
+        hotel: 'Hotel Leopold',
+    },
+    {
+        country: 'Spain',
+        city: 'Santa Cruz de Tenerife',
+        hotel: 'Apartment Sunshine',
+    },
+    {
+        country: 'Slowakia',
+        city: 'Vysokie Tatry',
+        hotel: 'Villa Kunerad',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hostel Friendship',
+    },
+    {
+        country: 'Indonesia',
+        city: 'Bali',
+        hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+        country: 'Netherlands',
+        city: 'Rotterdam',
+        hotel: 'King Kong Hostel',
+    },
+    {
+        country: 'Marocco',
+        city: 'Ourika',
+        hotel: 'Rokoko Hotel',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+];
+
+const prepareStringForSearch = (str, strToCompare) => str.toLocaleLowerCase().replaceAll(' ', '').includes(strToCompare);
+
+function search(searchRequest) {
+    const result = [];
+    const searchStringLowerCase = searchRequest.replaceAll(' ', '').toLowerCase();
+    for (let index = 0; index < data.length; index += 1) {
+        const { country, city, hotel } = data[index];
+        const concatedString = `${country}${city}${hotel}`;
+
+        if (prepareStringForSearch(concatedString, searchStringLowerCase)) {
+            result.push(`${country}, ${city}, ${hotel}`);
+        }
+    }
+
+    return result;
+}
+
+console.log(search('Germany Ber'));
+
+const date = '2020-11-26';
+const splitDate = date.split('-');
+const reverseDate = splitDate.reverse();
+const joinDate = reverseDate.join('.');
+
+console.log(joinDate);
+
+const newDate = date.split('-').reverse().join('.');
+console.log(newDate);
