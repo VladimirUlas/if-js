99c872749872d1314d275c9d8bf5b61a932a802c
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-03-29 19:46:11 +0300

    Homework7 (#7)
    * Homework7
    
    * Removed a mistake in 7 homework
    
    * homework_7
    
    * deleted eslint rule from main.js file
    
    Co-authored-by: nikitik-nikita <nik.ikraaa@gmail.com>

--- main.js
+++ main.js
@@ -362,3 +362,53 @@ function searchCountry() {
 }
 
 console.log(searchCountry());
+
+const obj1 = {
+    a: 'a',
+    b: {
+        a: 'a',
+        b: 'b',
+        c: {
+            a: 1,
+        },
+    },
+};
+const obj2 = {
+    b: {
+        c: {
+            a: 1,
+        },
+        b: 'b',
+        a: 'a',
+    },
+    a: 'a',
+};
+const obj3 = {
+    a: {
+        c: {
+            a: 'a',
+        },
+        b: 'b',
+        a: 'a',
+    },
+    b: 'b',
+};
+
+const deepEqual = (ObjectOne, objectTwo) => {
+    const isParametrsObject = typeof ObjectOne !== 'object' || typeof objectTwo !== 'object';
+    const isParametrsUndefined = ObjectOne === undefined || objectTwo === undefined;
+    const isParametrsNull = ObjectOne === null || objectTwo === null;
+    if (ObjectOne === objectTwo) return true;
+    if (isParametrsUndefined) return false;
+    if (isParametrsObject) return false;
+    if (isParametrsNull) return false;
+    const objectOneKeys = Object.keys(ObjectOne);
+    const objectTwoKeys = Object.keys(objectTwo);
+    if (objectOneKeys.length !== objectTwoKeys.length) return false;
+    for (const key of objectOneKeys) {
+        if (!objectTwoKeys.includes(key) || deepEqual(ObjectOne[key], objectTwo[key])) return true;
+    }
+    return false;
+};
+console.log(deepEqual(obj1, obj2));
+console.log(deepEqual(obj1, obj3));
