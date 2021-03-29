bff2c1ff16eb60288b360ebf112d802cf68f31c6
VladimirUlas <74644806+VladimirUlas@users.noreply.github.com>
2021-03-29 19:49:48 +0300

    Homework_8 (#8)
    * Homework_8
    
    * New homework_8
    
    Co-authored-by: nikitik-nikita <nik.ikraaa@gmail.com>

--- main.js
+++ main.js
@@ -412,3 +412,72 @@ const deepEqual = (ObjectOne, objectTwo) => {
 };
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
+
+const studentsData = [
+    {
+        firstName: 'Василий',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Java',
+    },
+    {
+        firstName: 'Иван',
+        lastName: 'Иванов',
+        admissionYear: 2018,
+        courseName: 'JavaScript',
+    },
+    {
+        firstName: 'Александр',
+        lastName: 'Федоров',
+        admissionYear: 2017,
+        courseName: 'Python',
+    },
+    {
+        firstName: 'Николай',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Android',
+    },
+];
+
+class User {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Student extends User {
+    constructor(firstName, lastName, admissionYear, courseName) {
+        super(firstName, lastName);
+        this.admissionYear = admissionYear;
+        this.courseName = courseName;
+    }
+
+    get course() {
+        return new Date().getFullYear() - this.admissionYear;
+    }
+}
+
+class Students {
+    constructor(students) {
+        this.students = students.map(({
+                                          firstName, lastName, admissionYear, courseName,
+                                      }) => new Student(firstName, lastName, admissionYear, courseName));
+    }
+
+    getInfo() {
+        this.students.sort((prev, next) => prev.course - next.course);
+        const arrgument = [];
+        this.students.forEach((item) => {
+            arrgument.push(`${item.fullName} - ${item.courseName}, ${item.course} курс`);
+        });
+        return arrgument;
+    }
+}
+const students = new Students(studentsData);
+console.log(students.getInfo());
